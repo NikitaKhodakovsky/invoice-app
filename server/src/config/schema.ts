178b50f5629de245d38d2cf1da0316b1635f4f86
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-18 20:31:01 +0300

    Server: Delete TestResolver

--- server/src/config/schema.ts
+++ server/src/config/schema.ts
@@ -2,11 +2,10 @@ import { buildSchemaSync, BuildSchemaOptions } from 'type-graphql'
 
 import { InvoiceResolvers } from '../modules/invoice'
 import { AuthResolvers } from '../modules/auth'
-import { TestResolver } from '../TestResolver'
 import { isProduction } from '../constants'
 
 export const schema = buildSchemaSync({
-	resolvers: [TestResolver, ...AuthResolvers, ...InvoiceResolvers] as BuildSchemaOptions['resolvers'],
+	resolvers: [...AuthResolvers, ...InvoiceResolvers] as BuildSchemaOptions['resolvers'],
 	emitSchemaFile: !isProduction,
 	dateScalarMode: 'isoDate'
 })
