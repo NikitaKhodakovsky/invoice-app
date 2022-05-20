51d607971ff437e761b26137e36116507d58ceac
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-20 15:30:48 +0300

    Server: Fix typings in context config function

--- server/src/config/context.ts
+++ server/src/config/context.ts
@@ -1,3 +1,4 @@
+import { ExpressContext } from 'apollo-server-express'
 import { ContextFunction } from 'apollo-server-core'
 
 import { Address, Invoice, OrderItem } from '../modules/invoice'
@@ -5,11 +6,12 @@ import { dataSource } from './dataSource'
 import { User } from '../modules/user'
 import { Context } from '../types'
 
-export const context: ContextFunction<Context> = ({ req, res }) => {
+export const context: ContextFunction<ExpressContext, Context> = ({ req, res }) => {
 	const orderItemRepository = dataSource.getRepository(OrderItem)
 	const invoiceRepository = dataSource.getRepository(Invoice)
 	const addressRepository = dataSource.getRepository(Address)
 	const userRepository = dataSource.getRepository(User)
+	const user = undefined // :)
 
 	return {
 		orderItemRepository,
@@ -17,6 +19,7 @@ export const context: ContextFunction<Context> = ({ req, res }) => {
 		addressRepository,
 		userRepository,
 		dataSource,
+		user,
 		req,
 		res
 	}
