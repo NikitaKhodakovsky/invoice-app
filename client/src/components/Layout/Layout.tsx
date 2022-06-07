345da9abe0e11d5ba4951fe033fe1df2be16f6d6
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-06-08 00:30:53 +0300

    Client: Add styles to Layout component

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import { Fragment } from 'react'
 
-import { Header } from './Header'
+import styles from './Layout.module.scss'
+
+import { Header } from '../Header'
 
 export function Layout() {
 	return (
 		<Fragment>
 			<Header />
-			<div className='container'>
+			<div className={`container ${styles.container}`}>
 				<Outlet />
 			</div>
 		</Fragment>
